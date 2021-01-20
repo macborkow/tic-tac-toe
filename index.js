2f55ec5cd304390104c1a66ed3bf684d83eeef51
macborkow <macborkow@gmail.com>
2021-01-20 11:23:25 +0100

    add gameBoard&displayController modules and player factory

---
+++ index.js
@@ -0,0 +1,15 @@
+const gameBoard = (() => {
+
+})();
+
+const displayController = (() => {
+
+})();
+
+const player = () => {
+    let moves = [];
+    const makeMove = x => {
+        moves.push(x);
+    }
+}
+
