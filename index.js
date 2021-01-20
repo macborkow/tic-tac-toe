7dd7ddd4debc527315a6f79e4831de0093b7dc7e
macborkow <macborkow@gmail.com>
2021-01-20 14:10:39 +0100

    Add drawing methods for displayController

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+
+
 const gameBoard = (() => {
 
 })();
 
 const displayController = (() => {
-
+    let displayBoard = [];
+    for(let i = 0; i < 9; ++i) {
+        displayBoard.push(document.getElementById('space'.concat(i)));
+    }
+    
+    const addX = (x) => { displayBoard[x].innerHTML = "X"; }
+    const addO = (x) => { displayBoard[x].innerHTML = "O"; }
+    const reset = () => { for(let element of displayBoard) { element.innerHTML = ""; }}
+    return {addX, addO, reset};
 })();
 
 const player = () => {
